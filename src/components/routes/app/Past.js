51dcb831a17920418f69ef3f81f6fbc20d9710a5
Ryan Yost <ryost217@gmail.com>
2019-07-06 10:38:15 -0500

    tweaks

--- src/components/routes/app/Past.js
+++ src/components/routes/app/Past.js
@@ -211,8 +211,8 @@ const Past = ({ pastMovies, group, user, selectedSeason }) => {
           a = a.releaseDate;
           b = b.releaseDate;
 
-          if (a > b) return 1;
-          if (b > a) return -1;
+          if (a > b) return -1;
+          if (b > a) return 1;
           return 0;
         })
         .map((movie, i) => {
